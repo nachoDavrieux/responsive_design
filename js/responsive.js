f16ce821b804424c18cddb134f0a9ef404868366
ignacio <ignaciodavrieux@gmail.com>
2023-10-14 19:17:48 -0300

    casi casi

---
+++ js/responsive.js
@@ -0,0 +1,21 @@
+const myCarouselElement = document.querySelector('#myCarousel')
+
+$(document).ready(function () {
+    $('#textCarousel').carousel({
+        interval: 2000
+    });
+});
+
+/*Mostrar los juegos populares*/ 
+document.addEventListener("DOMContentLoaded", function() {
+    var enlace = document.getElementById("smmdCarruselIr");
+    var elemento = document.getElementById("accordionFlushExample");
+
+    enlace.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        elemento.style.display = (elemento.style.display === "none") ? "block" : "none";
+    });
+});
+
+
+
